d155b9ab7f09448798e5fe6055ba0351a94590eb
Ekamjot <ekamjot.chugh1004@gmail.com>
2023-12-01 02:00:53 +0530

    Teacher dashboard Fixed

--- frontend/src/components/TeacherDashboard/TeacherEvent.jsx
+++ frontend/src/components/TeacherDashboard/TeacherEvent.jsx
@@ -3,9 +3,12 @@ import axios from "axios";
 import TeacherEventCard from "./TeacherEventCard/TeacherEventCard";
 import TeacherSidebar from "./TeacherSidebar/TeacherSidebar";
 import { motion } from "framer-motion";
+import dayjs from "dayjs";
 export default function TeacherEvent() {
   const [eventsData, setEventsData] = useState([]);
-  const currentDate = Date();
+  let currentDate = Date();
+  currentDate = dayjs(currentDate).format("MM/DD/YYYY");
+  const clubId = localStorage.getItem("clubId");
   const load_data = async () => {
     var response = "";
     response = await axios.get("http://localhost:8080/api/user/get-events", {
@@ -40,48 +43,61 @@ export default function TeacherEvent() {
             backgroundColor: "#071e3d",
             color: "white",
             width: "100%",
-          }}
-        >
-          <div className="row justify-content-center text-center mb-3 mt-5">
+          }}>
+          {/*<div className="row justify-content-center text-center mb-3 mt-5">
             <div className="col-lg-12 col-xl-12">
-              <h1 className="display-5" style={{ color: '#21e6c1', fontWeight:'400' }}>Past Events</h1>
+              <h1 className="display-5" style={{ color: '#21e6c1', fontWeight:'400' }}>Upcoming Events</h1>
             </div>
           </div>
           <div className="container mt-3 ml-5">
             <div className="row ">
               {eventsData?.map((event) => {
-                return currentDate >= event.date ? (
-                  <div
-                    className="col-12  col-md- col-lg-4"
-                    style={{ margin: "4%" }}
-                    
+                return currentDate < dayjs(event.date).format("MM/DD/YYYY") &&
+                event.clubId == clubId ? (
+                <div
+                  className="col-12  col-md- col-lg-3"
+                  style={{ margin: "4%" }}
+                >
+                  {" "}
+                  <motion.div
+                    className="box"
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{
+                      duration: 0.8,
+                      delay: 0.5,
+                      ease: [0, 0.71, 0.2, 1.01],
+                    }}
+                    // animate={{ x: [-100, 0] }}
+                    // transition={{ duration: 1 }}
                   >
-                    {" "}
-                        <TeacherEventCard event={event} occurence={"present"} />
-                      
+                    <div>
+                      <TeacherEventCard
+                        event={event}
+                        occurence={"present"}
+                      />
+                    </div>
+                  </motion.div>
                   </div>
                 ) : (
-                  <></>
+                  <> </>
                 );
               })}
             </div>
           </div>
-          <hr />
-          <h2
-            className="m-3"
-            style={{ borderBottom: "2px solid grey", padding: "2%" }}
-          >
-            Past Events
-          </h2>
-          <div className="container mt-3 ml-3">
+            <hr />*/}
+          <div className="row justify-content-center text-center mb-3 mt-5">
+            <div className="col-lg-12 col-xl-12">
+              <h1 className="display-5" style={{ color: '#21e6c1', fontWeight:'400' }}>Past Events</h1>
+            </div>
+          </div>
+          <div className="container mt-4 ml-2">
             <div className="row">
               {eventsData?.map((event) => {
-                return;
-                currentDate < event.date ? (
+                return event.clubId == clubId &&
                   <div
-                    className="col-12  col-md- col-lg-3"
-                    style={{ margin: "4%" }}
-                  >
+                    className="col-12  col-md-4 col-lg-4"
+                    style={{ margin: "4%" }}>
                     {" "}
                     <motion.div
                       className="box"
@@ -91,19 +107,10 @@ export default function TeacherEvent() {
                         duration: 0.8,
                         delay: 0.5,
                         ease: [0, 0.71, 0.2, 1.01],
-                      }}
-                      // animate={{ x: [-100, 0] }}
-                      // transition={{ duration: 1 }}
-                    >
-                      <div className="card ">
+                      }}>
                         <TeacherEventCard event={event} occurence={"past"} />
-                      </div>
                     </motion.div>
-                  </div>
-                ) : (
-                  <></>
-                );
-              })}
+                  </div> })}
             </div>
           </div>
         </div>
